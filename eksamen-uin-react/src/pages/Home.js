74c0423e72677c939308d5865c4a6479ea0d027e
komagnus <komagnus@hiof.no>
2021-04-16 13:49:53 +0200

    Feil i home.js

--- eksamen-uin-react/src/pages/Home.js
+++ eksamen-uin-react/src/pages/Home.js
@@ -1,7 +1,7 @@
 import Footer from "../components/footer";
 import Header from "../components/header"
-import { Main, MainContent, RelevantArticle } from "../styles/Style";
-import { AllArticles, AllContent, ArticleContent } from "../styles/HomeStyle";
+import { Main, MainContent, AllContent } from "../styles/Style";
+import { AllArticles, RelevantArticle, ArticleContent } from "../styles/HomeStyle";
 import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
 import sanityClient from "../utils/client.js";
