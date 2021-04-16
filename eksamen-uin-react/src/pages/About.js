f1a4a538db04699f9995985686f206101555cf19
Hassanvm <77785021+Hassanvm@users.noreply.github.com>
2021-04-16 14:24:20 +0200

    about us

--- eksamen-uin-react/src/pages/About.js
+++ eksamen-uin-react/src/pages/About.js
@@ -1,21 +1,49 @@
-import React from "react";
-import Header from '../components/header';
-import { AllContent, Main, MainContent, } from "../styles/Style";
-import Footer from '../components/footer';
+import React, { useEffect, useState } from "react";
+import sanityClient from "../utils/client.js";
+import Header from "../components/header"
+import Footer from "../components/footer";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
 
-const About = () => {
-return (
-        <>
-        <Main>
-            <AllContent>
-                <MainContent>
-                    <Header/>
-                    <h1>Om oss side</h1>
-                    <Footer/>
-                </MainContent>
-            </AllContent>
-        </Main>
-        </>
-    );
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source)
 }
-export default About;
+
+export default function About() {
+    const [author, setAuthor] = useState(null) ;
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "author"]{
+        name,
+        bio,
+        "authorImage": image.asset->url
+        }`).then((data) => setAuthor(data[0]))
+        .catch(console.error);
+    }, []);
+
+    if (!author) return <div> Laster...</div>;
+
+    return (
+
+        <main>
+        <Header/>
+
+        <img src={urlFor(author.authorImage).url()} alt={author.name}
+        style={{height: "250px", width: "250px"}}/>
+
+        <div>
+
+        <h1> Forfatter navn: {""} </h1>
+        <span> {author.name}</span>
+        
+        <div>
+        <BlockContent blocks={author.bio} projectId="kggawxgp" dataset="production" />
+        </div>
+        </div>
+        
+        <Footer/>
+
+        </main>
+);
+    }
