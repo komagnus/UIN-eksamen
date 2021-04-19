8b7bacd1e0664f20dc6d01e0b527bd953766ce30
Blackdog2118 <78914124+Blackdog2118@users.noreply.github.com>
2021-04-20 00:31:40 +0200

    Comit KontaktInfo 20.04

---
+++ eksamen-uin-react/src/styles/ContactStyle.js
@@ -0,0 +1,96 @@
+import styled from "styled-components";
+export const KontaktossSide = styled.section`
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(4, 1fr) 0.5fr;
+    grid-column-gap: 30px;
+    grid-row-gap: 30px;
+`;
+export const KontaktossWrapper = styled.section`
+    { grid-area: 1 / 1 / 2 / 6; }
+    display: flex;
+    justify-content: center;
+    justify-content:space-around;
+    padding-top:30px;
+    }
+`;
+export const Kontaktinfocontent = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    justify-content:space-around;
+    flex-direction: column;
+    border: 2px solid #FFFFFF	;
+    width: 30%;
+    & > li {
+        display:flex;
+        
+        color: #FFFFFF
+    }
+    & > h1 {
+        font-size: 1vw;
+        color: #FFFFFF
+    }
+`;
+export const KontaktRingoss = styled.section`
+    display: flex;
+    justify-content: center;
+    justify-content:space-around;
+    border: 2px solid #FFFFFF	;
+    width 50%;
+    }
+`;
+
+
+export const KontaktossSkjemaWrapper = styled.section`
+    { grid-area: 2 / 1 / 5 / 6; }
+    display:flex;
+    justify-content: center;
+    }
+`;
+export const KontaktossSkjema = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items:center;
+    width 80%; 
+    border: 2px solid #FFFFFF	;
+    & > h1 {
+        font-size: 1vw;
+        color: #FFFFFF
+    }
+    & > button {
+        display: flex;
+        justify-content:center;
+        width: 10%;
+    }
+    & > textarea {
+        height: 30%;
+        width:50%;
+    }
+    }
+`;
+export const KontaktossNyhetsbrevWrapper = styled.section`
+    { grid-area: 5 / 1 / 6 / 6; }
+    display:flex;
+    justify-content: center;
+    padding-bottom:30px;
+    }
+}
+`;
+export const KontaktossNyhetsbrev = styled.section`
+    display:flex;
+    justify-content: center;
+    justify-content: space-around;
+    align-items:center;
+    width: 80%;
+    border: 2px solid #FFFFFF	;
+    }
+    & > h1 {
+        font-size: 1vw;
+        color: #FFFFFF
+    }
+    & > input{
+        height: 30%;
+        width:20%;
+    }
+`;
