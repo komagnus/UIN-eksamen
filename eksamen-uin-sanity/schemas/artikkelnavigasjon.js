3932ffc19b7ab1232f3830642729cf8f3ea10980
Hassanvm <77785021+Hassanvm@users.noreply.github.com>
2021-04-20 15:55:23 +0200

    artikkelnavigasjon

---
+++ eksamen-uin-sanity/schemas/artikkelnavigasjon.js
@@ -0,0 +1,59 @@
+export default {
+
+    title: 'Navigation',
+    name: 'artikkelnavigasjon',
+    type: 'document',
+  
+    fields: [
+      {
+          
+        title: 'Navn på den første navigasjonen',
+        name: 'navigationname1',
+        type: 'string',
+
+      },
+      {
+        title: 'Navn på den andre navigasjonen',
+        name: 'navigationname2',
+        type: 'string',
+      },
+      {
+        title: 'Navn på den tredje navigasjonen',
+        name: 'navigationname3',
+        type: 'string',
+
+      },
+      {
+        title: 'Navn på den fjerde navigasjonen',
+        name: 'navigationname4',
+        type: 'string',
+
+    },
+    {
+      title: 'Navn på den femte navigasjonen',
+      name: 'navigationname5',
+      type: 'string',
+
+    },
+    {
+      title: 'Navn på den sjette navigasjonen',
+      name: 'navigationname6',
+      type: 'string',
+
+    },
+    {
+      title: 'Navn på den syvende navigasjonen',
+      name: 'navigationname7',
+      type: 'string',
+        
+      },
+      ]
+
+      
+
+      
+      
+      
+  }
+  
+  
