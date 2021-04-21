d2fa2f072ce3a2582b43c8381e6305b4dbfda67a
Blackdog2118 <78914124+Blackdog2118@users.noreply.github.com>
2021-04-21 15:11:31 +0200

    About Style

--- eksamen-uin-react/src/styles/About.js
+++ eksamen-uin-react/src/styles/About.js
@@ -2,13 +2,15 @@ import styled from "styled-components";
 export const AboutContent = styled.section`
     display:flex;
     justify-content: center;
-    justify-content: space-between;
+    justify-content: space-around;
     padding-bottom:30px;
     flex-wrap: wrap
     }
     & > .hoved {
         display: flex;
         flex-direction: column;
+        max-width:250px;
+        padding:10px;
     }
 
 }
